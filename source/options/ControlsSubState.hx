69401062f8ce814ede817dc965cd5d20c779d2b3
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-03-29 13:24:30 +0300

    Update ControlsSubState.hx

--- source/options/ControlsSubState.hx
+++ source/options/ControlsSubState.hx
@@ -197,6 +197,10 @@ class ControlsSubState extends MusicBeatSubstate {
 			}
 		}
 		changeSelection();
+
+                #if android
+	        addVirtualPad(FULL, A_B);
+                #end
 	}
 
 	var leaving:Bool = false;
