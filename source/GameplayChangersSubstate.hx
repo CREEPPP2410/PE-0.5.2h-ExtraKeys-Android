3d922d0ea88f2e813ce6841a1e244a32a7fa2340
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-03-29 13:10:01 +0300

    Update GameplayChangersSubstate.hx

--- source/GameplayChangersSubstate.hx
+++ source/GameplayChangersSubstate.hx
@@ -285,7 +285,7 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 				}
 			}
 
-			if(controls.RESET)
+			if(controls.RESET #if android || _virtualpad.buttonC.justPressed #end)
 			{
 				for (i in 0...optionsArray.length)
 				{
