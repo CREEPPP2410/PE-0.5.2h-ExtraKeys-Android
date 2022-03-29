b174850cd4cc8dc6e4cdf52585ebd2626b096f07
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-03-29 13:12:09 +0300

    Update BaseOptionsMenu.hx

--- source/options/BaseOptionsMenu.hx
+++ source/options/BaseOptionsMenu.hx
@@ -238,7 +238,7 @@ class BaseOptionsMenu extends MusicBeatSubstate
 				}
 			}
 
-			if(controls.RESET)
+			if(controls.RESET #if android || _virtualpad.buttonC.justPressed #end)
 			{
 				for (i in 0...optionsArray.length)
 				{
