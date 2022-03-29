9e336f34058f29c959a834aa30fa16d4d39a02e6
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-03-29 13:25:17 +0300

    Update BaseOptionsMenu.hx

--- source/options/BaseOptionsMenu.hx
+++ source/options/BaseOptionsMenu.hx
@@ -124,6 +124,10 @@ class BaseOptionsMenu extends MusicBeatSubstate
 
 		changeSelection();
 		reloadCheckboxes();
+
+                #if android
+	        addVirtualPad(FULL, A_B_C);
+                #end
 	}
 
 	public function addOption(option:Option) {
