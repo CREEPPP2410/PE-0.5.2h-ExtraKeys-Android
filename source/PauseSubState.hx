9669060acf297fa7b6415fb3f0b153b6966c05cb
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-03-29 13:31:15 +0300

    Update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -20,7 +20,7 @@ class PauseSubState extends MusicBeatSubstate
 	var grpMenuShit:FlxTypedGroup<Alphabet>;
 
 	var menuItems:Array<String> = [];
-	var menuItemsOG:Array<String> = ['Resume', 'Restart Song', 'Change Difficulty', 'Exit to menu'];
+	var menuItemsOG:Array<String> = ['Resume', 'Restart Song', 'Change Difficulty', 'Chart Editor', 'Exit to menu'];
 	var difficultyChoices = [];
 	var curSelected:Int = 0;
 
@@ -133,6 +133,18 @@ class PauseSubState extends MusicBeatSubstate
 
 		regenMenu();
 		cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
+
+                #if android
+                if (PlayState.chartingMode)
+                {
+                        addVirtualPad(FULL, A);
+                }
+                else
+                {
+                        addVirtualPad(UP_DOWN, A);
+                }
+                addPadCamera();
+                #end
 	}
 
 	var holdTime:Float = 0;
@@ -248,6 +260,9 @@ class PauseSubState extends MusicBeatSubstate
 					PlayState.instance.botplayTxt.visible = PlayState.instance.cpuControlled;
 					PlayState.instance.botplayTxt.alpha = 1;
 					PlayState.instance.botplaySine = 0;
+                                case "Chart Editor":
+		                        PlayState.chartingMode = true;
+                                        MusicBeatState.switchState(new editors.ChartingState());
 				case "Exit to menu":
 					PlayState.deathCounter = 0;
 					PlayState.seenCutscene = false;
